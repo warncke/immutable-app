e0e3a54618edec41e57e0f8efa68ca90ae084c96
Ersun Warncke <ersun.warncke@gmail.com>
2017-01-24 20:13:21 -0400

    start/stop http server with express app

--- test/immutable-app.js
+++ test/immutable-app.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const Promise = require('bluebird')
 const app = require('../lib/immutable-app')
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
@@ -9,13 +8,46 @@ const httpClient = require('immutable-http-client')
 chai.use(chaiAsPromised)
 const assert = chai.assert
 
-const testPort = 37591
-const testUrl = 'http://localhost:'+testPort
-
 describe('immutable-app', function () {
 
-    beforeEach(function () {
-        app.reset()
+    beforeEach(async function () {
+        // catch async errors
+        try {
+            // reset global app config
+            await app.reset()
+            // set configuration for testing
+            app.set({
+                // do not exit on listen errors
+                exit: false,
+                // do not log
+                log: false,
+            })
+        }
+        catch (err) {
+            throw err
+        }
+    })
+
+    it('should start new app', async function () {
+        // catch async errors
+        try {
+            // start server
+            await app.start()
+        }
+        catch (err) {
+            assert.ifError(err)
+        }
+    })
+
+    it('should restart app', async function () {
+        // catch async errors
+        try {
+            // start server
+            await app.start()
+        }
+        catch (err) {
+            assert.ifError(err)
+        }
     })
 
 })
