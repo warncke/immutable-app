1db516b67344922407840e21df87d39afd24a9c8
Ersun Warncke <ersun.warncke@gmail.com>
2017-01-29 13:01:21 -0400

    add database/model init and fix config merge for nested objects

---
+++ test/app/foo/foo.model.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const ImmutableCoreModel = require('immutable-core-model')
+
+module.exports = new ImmutableCoreModel({
+    name: 'foo',
+})
--- test/immutable-app.js
+++ test/immutable-app.js
@@ -1,26 +1,53 @@
 'use strict'
 
+const ImmutableDatabaseMariaSQL = require('immutable-database-mariasql')
 const immutableApp = require('../lib/immutable-app')
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 const httpClient = require('immutable-http-client')
+const immutable = require('immutable-core')
 
 chai.use(chaiAsPromised)
 const assert = chai.assert
+
+const dbHost = process.env.DB_HOST || 'localhost'
+const dbName = process.env.DB_NAME || 'test'
+const dbPass = process.env.DB_PASS || ''
+const dbUser = process.env.DB_USER || 'root'
+
+// use the same params for all connections
+const connectionParams = {
+    charset: 'utf8',
+    db: dbName,
+    host: dbHost,
+    password: dbPass,
+    user: dbUser,
+}
 
 describe('immutable-app', function () {
 
     var app
 
+    // create database connection to use for testing
+    var database = new ImmutableDatabaseMariaSQL(connectionParams)
+
     beforeEach(async function () {
         // catch async errors
         try {
+            // reset immutable modules
+            immutable.reset()
+            // drop any test tables
+            await database.query('DROP TABLE IF EXISTS foo'),
             // reset global app config
             await immutableApp.reset()
             // create new app instance
             app = immutableApp('test-app')
             // set configuration for testing
             app.config({
+                // set default database
+                database: {
+                    default: database,
+                },
                 // do not exit on listen errors
                 exit: false,
                 // do not log
